3678cd2560805ddb3c48bd60d21f9d8fd913e85c
Harikrishnan N <101572173+anideator@users.noreply.github.com>
2023-10-09 00:00:13 +0530

    Delete TrendMeld directory

--- TrendMeld/config/dbconnectivity.php
+++
@@ -1,7 +0,0 @@
-<?php
-$HN = "localhost";
-$UN = "root";
-$PW = "";
-$DB = "taskmanager";
-
-$conn = new mysqli($HN, $UN, $PW, $DB);
--- TrendMeld/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once './tm_home.php';
-session_start();
-session_destroy();
-$time = date("h:i:s d-m-Y");
-$flag = new update($id, $alias, $time);
-if ($flag) echo "<script>alert('Logged out')</script>";
